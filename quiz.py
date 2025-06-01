ec009e11abb3b35862c918988a6f346fab670196
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-01 20:46:14 +0200

    Add QUIZ_FILE constant to store the quiz CSV filename

--- quiz.py
+++ quiz.py
@@ -1,1 +1,9 @@
 import csv
+
+QUIZ_FILE = questions.csv
+
+# def read_questions(csv_file):
+#     questions = []
+#     with open(csv_file, newline=' ', encoding='utf-8') as file:
+#         reader = csv.reader(file)
+        
