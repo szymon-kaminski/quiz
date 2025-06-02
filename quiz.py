1b86380430927451a4385246d9501d47c47f6429
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-02 12:30:20 +0200

    Add run_quiz function to handle quiz logic

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ QUIZ_FILE = "questions.csv"
 
 def read_questions(csv_file):
     questions = []
-    with open(csv_file, newline=' ', encoding='utf-8') as file:
+    with open(csv_file, newline='', encoding='utf-8') as file:
         reader = csv.reader(file)
         for row in reader:
             if len(row) == 2:
@@ -18,6 +18,28 @@ def read_questions(csv_file):
                     continue 
                 questions.append((question_text, answer_bool))
     return questions
+
+
+def read_quiz(questions):
+    score = 0
+    for i, (question, correct_answer) in enumerate(questions, start=1):
+        print(f"\nQuestion {i}: {qusetion}")
+        user_input = input("Answer (true/false): ").strip().lower()
+        
+        if user_input == true:
+            user_answer = True
+        elif user_input == false:
+            user_answer = False
+        else:
+            print("Incorrect aswer!")
+            continue
+
+        if user_answer == correct_answer:
+            print("Correct answer!")
+            score += 1
+        else:
+            print("Incorrect answer")
+    print(f"\nYour Score: {score} / {len(questions)}")
 
 
 def main():
