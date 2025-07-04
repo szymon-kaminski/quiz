3884c46cf333e3bbf2e7023c9aa0f57c9586c06b
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-07-04 22:58:36 +0200

    Add test-to-pass for read_questions() with test CSV

---
+++ tests/test_quiz.py
@@ -0,0 +1,10 @@
+from quiz import read_questions
+
+def test_read_questions_returns_expected_output():
+    test_file = "tests/test_questions.csv"
+    expected = [
+        ("Is Python a programming language?", True),
+        ("Is the sky green?", False)
+    ]
+    result = read_questions(test_file)
+    assert result == expected
