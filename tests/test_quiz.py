8006b67e5648ba54c5dadee9c4845597a1d0edd0
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-07-07 13:10:08 +0200

    Add test for run_quiz using mocked input

--- tests/test_quiz.py
+++ tests/test_quiz.py
@@ -1,7 +1,9 @@
-import sys
+import builtins
 import os
+import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from quiz import run_quiz
 from quiz import read_questions
 
 def test_read_questions_returns_expected_output():
@@ -23,3 +25,18 @@ def test_read_questions_should_fail():
     result = read_questions(test_file)
     assert result == wrong_expected
 
+
+def test_run_quiz_with_mock_input(monkeypatch, capsys):
+    questions = [
+        ("Python is a programing language.", True),
+        ("The Sun is cold.", False),
+    ]
+
+    user_inputs = iter(["true", "false"])
+    monkeypatch.setattr(builtins, "input", lambda _: next(user_inputs))
+
+    run_quiz(questions)
+
+    output = capsys.readouterr().out
+
+    assert "Your score: 2 / 2" in output
